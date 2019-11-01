0b454964d7cc5a16761745746e5966a8c0152a76
Max Reinmueller <maxreinmueller@gmail.com>
2019-11-01 15:03:15 -0700

    set up component and controller files

---
+++ src/components/TripOverviewController.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PlaceToStay from "./PlaceToStay"
+
+class TripOverviewController extends Component {
+
+
+render() {
+    return (
+        <PlaceToStay />
+    );
+};
+}
+
+export default TripOverviewController;
